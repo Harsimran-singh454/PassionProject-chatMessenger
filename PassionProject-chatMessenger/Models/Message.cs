35acbd87a312284ae79140884a128c4d83a55ad4
Harsimran-singh454 <n01562454@humber.ca>
2023-06-05 12:41:18 -0400

    Add project files.

---
+++ PassionProject-chatMessenger/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassionProject_chatMessenger.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string message { get; set; }
+        public int
+    }
+}
